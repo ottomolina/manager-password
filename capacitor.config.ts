9d858f219c63574a1ff06fa3ff7292915853f349
ottoniel <ottonielmolina@gmail.com>
2024-06-26 00:56:47 -0600

    chore: se crea proyecto con sus dependencias.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'manager-password',
+  webDir: 'www'
+};
+
+export default config;
