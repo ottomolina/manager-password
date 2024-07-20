b3dd1e327b11655431114d7172462892273ae643
ottoniel <ottonielmolina@gmail.com>
2024-07-19 23:12:03 -0600

    feat(util): se agrega service para navegación y propiedad relativa a formularios en app.service.

--- src/app/util/app.service.ts
+++ src/app/util/app.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { AlertController, LoadingController, ModalController, ToastButton, ToastController } from '@ionic/angular';
 import { ComponentProps, ComponentRef } from '@ionic/core';
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
+  public form: FormGroup;
   private isLoading = false;
   private showToast = false;
 
@@ -91,6 +93,48 @@ export class AppService {
     });
     await modal.present();
     return modal;
+  }
+
+  /**
+   * Obtiene una descripción del error dentro del control del formulario
+   * @param name: nombre del control del formulario
+   * @param patron: (opcional) en caso de validar pattern indicar un texto de ejemplo que cumpla el patrón
+   * @returns: string con el error del control del formulario
+  */  
+  public getErrorFormControl(name: string, patron?: string): string {
+    const errors = this.form.controls[name].errors!;
+    if(errors['required']) {
+      return 'Este campo es obligatorio.';
+    }
+    if(errors['minlength']) {
+      const error = this.form.controls[name].errors?.['minlength'];
+      return `El valor ingresado debe tener al menos ${error.requiredLength} caracteres.`;
+    }
+    if(errors['min']) {
+      const error = this.form.controls[name].errors?.['min'];
+      return `El valor mínimo de este campo es ${error.min}.`;
+    }
+    if(errors['max']) {
+      const error = this.form.controls[name].errors?.['max'];
+      return `El valor máximo de este campo es ${error.max}.`;
+    }
+    if(errors['maxlength']) {
+      const error = this.form.controls[name].errors?.['maxlength'];
+      return `El valor ingresado no debe tener más de ${error.requiredLength} caracteres.`;
+    }
+    if(errors['pattern']) {
+      return `El valor ingresado es inválido${patron ? `, texto de ejemplo: ${patron}` : ''}`;
+    }
+    return '';
+  }
+
+  /**
+   * Verifica si un control form contiene errores
+   * @param name: nombre del control del formulario
+   * @returns: boolean indicando si el control form contiene errores
+   */
+  public hasErrorFormControl(name: string): boolean {
+    return this.form.controls[name].errors !== null;
   }
 
 }
