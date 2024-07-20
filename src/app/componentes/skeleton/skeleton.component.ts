2fc812e5fe86809d102f6224181cff9147e8899f
ottoniel <ottonielmolina@gmail.com>
2024-07-19 23:10:45 -0600

    feat(skeleton): se agrega componente skeleton indicador de que página está cargando.

---
+++ src/app/componentes/skeleton/skeleton.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton',
+  templateUrl: './skeleton.component.html',
+  styleUrls: ['./skeleton.component.scss'],
+})
+export class SkeletonComponent  implements OnInit {
+  @Input() cargando: boolean;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
